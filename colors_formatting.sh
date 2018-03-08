aabda32933ca27c89a41eabab64017e059d80121
zrthstr <zrth1k@gmail.com>
2018-03-08 13:58:16 +0100

    Create colors_formatting.sh

---
+++ colors_formatting.sh
@@ -0,0 +1,46 @@
+###
+###    Source: https://misc.flogisoft.com/bash/tip_colors_and_formatting 
+###
+
+
+echo -e "\e[38;5;82mHello \e[38;5;198mWorld"
+
+for i in {16..21} {21..16} ; do echo -en "\e[38;5;${i}m#\e[0m" ; done ; echo
+
+echo -e "\e[40;38;5;82m Hello \e[30;48;5;82m World \e[0m"
+
+for i in {16..21} {21..16} ; do echo -en "\e[48;5;${i}m \e[0m" ; done ; echo
+
+echo -e "\e[1;4mBold and Underlined"
+
+echo -e "\e[1;31;42m Yes it is awful \e[0m"
+
+
+#Background
+for clbg in {40..47} {100..107} 49 ; do
+	#Foreground
+	for clfg in {30..37} {90..97} 39 ; do
+		#Formatting
+		for attr in 0 1 2 4 5 7 ; do
+			#Print the result
+			echo -en "\e[${attr};${clbg};${clfg}m ^[${attr};${clbg};${clfg}m \e[0m"
+		done
+		echo #Newline
+	done
+done
+
+
+
+for fgbg in 38 48 ; do # Foreground / Background
+    for color in {0..255} ; do # Colors
+        # Display the color
+        printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+        # Display 6 colors per lines
+        if [ $((($color + 1) % 6)) == 4 ] ; then
+            echo # New line
+        fi
+    done
+    echo # New line
+done
+
+exit 0
